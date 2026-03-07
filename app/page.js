9f2d0180c4147e5adc6a0640069c54996e9f6373
smacf <stewmacfarlane@icloud.com>
2026-03-07 01:05:00 +0000

    Homepage progress

--- app/page.js
+++ app/page.js
@@ -1,15 +1,18 @@
-function Header({ title }) {
-  return <h1>{title ? title : 'Default title'}</h1>;
-}
- 
-export default function HomePage() {
+function GetCoverScreen () {
   return (
-    <div className="homePage">
-      <div className="homeImg"></div>
       <div className='coverScreen'>
         <h1 className="title">Heather Hotel & Spa</h1>
         <h3 className="subTitle">Immerse Yourself in Scotland</h3>
       </div>
+  )
+}
+
+export default function HomePage() {
+  return (
+    <div className="homePage">
+    <div className="homeImg"></div>
+    <GetCoverScreen />
+    <div className="myRect"></div>
     <div>
       <p className="page">
         Hello!
--- app/src/navBar/nav.js
+++ app/src/navBar/nav.js
@@ -6,12 +6,12 @@ function handleClick(msg) {
     console.log("Button pressed " + msg)
 }
 
-function GetButton({name, msg}) {
+function GetButton({name, msg, className}) {    
     return (
         <div>
             <button 
                 onClick={() => handleClick(msg)} 
-                className="navButton"
+                className={className}
             >
                 {name}
             </button>
@@ -26,28 +26,26 @@ function getNav() {
 
                 <div className="leftBar">
                     <Link href="/">
-                        <GetButton name="Home" msg="Home clicked!" />
+                        <GetButton name="Home" msg="Home clicked!" className="navButton"/>
                     </Link>
-                    <Link href="/src/pages/page2">
-                        <GetButton name="=" msg="Page2 clicked!" />
-                    </Link>
+                    <GetButton name="=" msg="Page2 clicked!" className="navButton"/>
                 </div>
 
                 <div className="rightBar">
-                    <Link href="/src/pages/page2">
-                        <GetButton name="Rooms" msg="Page3 clicked!" />
+                    <Link href="/src/pages/RoomsPage">
+                        <GetButton name="Rooms" msg="Page3 clicked!" className="navButton"/>
                     </Link>
-                    <Link href="/src/pages/page2">
-                        <GetButton name="Wellness" msg="Page3 clicked!" />
+                    <Link href="/src/pages/WellnessPage">
+                        <GetButton name="Wellness" msg="Page3 clicked!" className="navButton"/>
                     </Link>
-                    <Link href="/src/pages/page2">
-                        <GetButton name="Experiences" msg="Page3 clicked!" />
+                    <Link href="/src/pages/ExperiencesPage">
+                        <GetButton name="Experiences" msg="Page3 clicked!" className="navButton"/>
                     </Link>
-                    <Link href="/src/pages/page2">
-                        <GetButton name="Dining" msg="Page3 clicked!" />
+                    <Link href="/src/pages/DiningPage">
+                        <GetButton name="Dining" msg="Page3 clicked!" className="navButton"/>
                     </Link>
-                    <Link href="/src/pages/page2">
-                        <GetButton name="Book" msg="Page3 clicked!" />
+                    <Link href="/src/pages/BookPage">
+                        <GetButton name="Book" msg="Page3 clicked!" className="bookButton"/>
                     </Link>
                 </div>
             </div>
